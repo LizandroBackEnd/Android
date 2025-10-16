4a824ded2d4fe9ea812746d30ba48f446bba9c37
LizandroBackEnd <codeversebackend@gmail.com>
2025-10-16 10:17:47 -0600

    ci(States): Added the first form for the @Composable

--- States/app/src/main/java/com/lizdev/estados/MainActivity.kt
+++ States/app/src/main/java/com/lizdev/estados/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lizdev.estados.ui.theme.EstadosTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +32,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EstadosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                StateSample1()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EstadosTheme {
-        Greeting("Android")
+fun StateSample1() {
+    var text = remember { mutableStateOf("") }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField (
+            value = text.value,
+            onValueChange = { text.value = it },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer (Modifier.height(6.dp))
+        Text (
+            text = text.value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Cyan)
+                .padding(10.dp)
+        )
+        Spacer (Modifier.height(6.dp))
+        Button (
+            onClick = {
+                text.value = ""
+            },
+            enabled = text.value.isNotEmpty(),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text (text = "Clear")
+        }
     }
 }
