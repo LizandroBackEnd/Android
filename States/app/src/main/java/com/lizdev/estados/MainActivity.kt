74c3ef0ac87b4faafc623eecc4b445c7d9a2c343
LizandroBackEnd <codeversebackend@gmail.com>
2025-10-17 23:49:29 -0600

    ci(States): Added the five form the states

--- States/app/src/main/java/com/lizdev/estados/MainActivity.kt
+++ States/app/src/main/java/com/lizdev/estados/MainActivity.kt
@@ -17,8 +17,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,7 +35,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EstadosTheme {
-                StateSample1()
+                //StateSample1()
+                //StateSample2()
+                //StateSample3()
+
+                //var text by rememberSaveable() { mutableStateOf("") }
+                //StateSample4(value = text, onValueChange = { text = it})
+
+                val (value, onValueChange ) = rememberSaveable() { mutableStateOf("") }
+                StateSample5(value = value, onValueChange = onValueChange)
+
             }
         }
     }
@@ -67,6 +79,148 @@ fun StateSample1() {
                 text.value = ""
             },
             enabled = text.value.isNotEmpty(),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text (text = "Clear")
+        }
+    }
+}
+
+@Composable
+fun StateSample2() {
+    var text by remember { mutableStateOf("") }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement =  Arrangement.Center
+    ) {
+        OutlinedTextField (
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer (modifier = Modifier.height(6.dp))
+        Text (
+            text = text,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Cyan)
+                .padding(10.dp)
+        )
+        Spacer (modifier = Modifier.height(6.dp))
+        Button (
+            onClick = {
+                text = ""
+            },
+            enabled = text.isNotEmpty(),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Clear")
+        }
+    }
+}
+
+@Composable
+fun StateSample3 () {
+    var text by rememberSaveable { mutableStateOf("") }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField (
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer (modifier = Modifier.height(6.dp))
+        Text (
+            text = text,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Cyan)
+                .padding(10.dp)
+        )
+        Spacer (modifier = Modifier.height(6.dp))
+        Button (
+            onClick = {
+                text = ""
+            },
+            enabled = text.isNotEmpty(),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Clear")
+        }
+    }
+}
+
+@Composable
+fun StateSample4 (value: String, onValueChange: (String)-> Unit) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField (
+            value = value,
+            onValueChange = { onValueChange },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer (modifier = Modifier.height(6.dp))
+        Text (
+            text = value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Cyan)
+                .padding(10.dp)
+        )
+        Spacer (modifier = Modifier.height(6.dp))
+        Button (
+            onClick = {
+                onValueChange("")
+            },
+            enabled = value.isNotEmpty(),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text (text = "Clear")
+        }
+    }
+}
+
+@Composable
+fun StateSample5 (value: String, onValueChange: (String) -> Unit) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField (
+            value = value,
+            onValueChange = { onValueChange(it) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer (modifier = Modifier.height(6.dp))
+        Text (
+            text = value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Cyan)
+                .padding(10.dp)
+        )
+        Spacer (modifier = Modifier.height(6.dp))
+        Button (
+            onClick = {
+                onValueChange("")
+            },
+            enabled = value.isNotEmpty(),
             modifier = Modifier.fillMaxWidth()
         ) {
             Text (text = "Clear")
