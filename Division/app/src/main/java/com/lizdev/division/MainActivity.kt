894ac0621a35fb3d8ed6087923ee9eb403c81a32
LizandroBackEnd <codeversebackend@gmail.com>
2025-10-19 00:31:41 -0600

    ci(Division): Added the exercice division

---
+++ Division/app/src/main/java/com/lizdev/division/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.lizdev.division
+
+import android.R.attr.onClick
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.lizdev.division.ui.theme.DivisionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DivisionTheme {
+                Division()
+            }
+        }
+    }
+}
+
+@Composable
+fun Division () {
+    var a by remember { mutableStateOf("") }
+    var b by remember { mutableStateOf("") }
+    var context = LocalContext.current
+    var focusManager = LocalFocusManager.current
+    var resultado by remember { mutableStateOf("") }
+
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        Text (
+            text = "División",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Image (
+            painter = painterResource(R.drawable.math),
+            contentDescription = "DIVISION",
+            modifier = Modifier.size(150.dp)
+        )
+        OutlinedTextField (
+            value = a,
+            onValueChange = { a = it },
+            label  = { Text("A")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        OutlinedTextField (
+            value = b,
+            onValueChange = { b = it },
+            label  = { Text("B")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Row (
+            modifier = Modifier.padding(12.dp)
+        ) {
+            Button (
+                onClick = {
+                    val numA = a.toDoubleOrNull()
+                    val numB = b.toDoubleOrNull()
+
+                    if(numA == null || numB == null) {
+                        Toast.makeText(context, "Faltan ingresar datos", Toast.LENGTH_LONG).show()
+                    } else if (numB == 0.0) {
+                        Toast.makeText(context, "No se puede dividir entre cero", Toast.LENGTH_LONG).show()
+                    } else {
+                        resultado = String.format("%.2f", numA / numB)
+                        Toast.makeText(context, "Resultado: $resultado", Toast.LENGTH_LONG).show()
+                    }
+                },
+            ) {
+                Text (text = "Dividir")
+            }
+            Spacer (modifier = Modifier.width(16.dp))
+            Button (
+                onClick = {
+                    a = ""
+                    b = ""
+                    resultado = ""
+                    focusManager.moveFocus(FocusDirection.Up)
+                }
+            ) {
+                Text (text = "Limpiar")
+            }
+
+        }
+
+    }
+
+}
