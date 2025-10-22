1ee7d5407170eb58051d3f8ab49a457f9e6aa82c
LizandroBackEnd <codeversebackend@gmail.com>
2025-10-22 11:44:14 -0600

    ci(LayoutsBox): Added the example the Layout Box

--- Box/app/src/main/java/com/lizdev/box/MainActivity.kt
+++ Box/app/src/main/java/com/lizdev/box/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lizdev.box.ui.theme.BoxTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +28,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BoxTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyBox()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BoxTheme {
-        Greeting("Android")
+fun MyBox () {
+    Box (modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Box (modifier = Modifier
+            .width(20.dp)
+            .height(30.dp)
+            .background(Color.Red)
+            .verticalScroll(rememberScrollState())
+        ) {
+            Text (text = "Hola Hola Hola")
+        }
     }
 }
