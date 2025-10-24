a9895b0e9aaab882fb1341ff5f07de7c478e7de8
LizandroBackEnd <codeversebackend@gmail.com>
2025-10-23 19:16:42 -0600

    ci(ConstraintLayout): Added the example the Constraint Layout

---
+++ ConstraintLayout/app/src/main/java/com/lizdev/constraintlayout/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.lizdev.constraintlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.lizdev.constraintlayout.ui.theme.ConstraintLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ConstraintLayoutTheme {
+                MyConstraintLayout()
+
+            }
+        }
+    }
+}
+
+@Composable
+fun MyConstraintLayout () {
+    ConstraintLayout (modifier = Modifier.fillMaxSize()){
+        val (boxRed, boxMagenta, boxBlack, boxCyan, boxYellow) = createRefs()
+
+    }
+}
