90b0f94633a8c24f83d2b4db37de3d370fa92445
LizandroBackEnd <codeversebackend@gmail.com>
2025-10-23 19:17:42 -0600

    ci(ConstraintLayout): Added the example the Constraint Layout

--- ConstraintLayout/app/src/main/java/com/lizdev/constraintlayout/MainActivity.kt
+++ ConstraintLayout/app/src/main/java/com/lizdev/constraintlayout/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.lizdev.constraintlayout.ui.theme.ConstraintLayoutTheme
 
@@ -21,7 +26,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             ConstraintLayoutTheme {
                 MyConstraintLayout()
-
             }
         }
     }
@@ -31,6 +35,29 @@ class MainActivity : ComponentActivity() {
 fun MyConstraintLayout () {
     ConstraintLayout (modifier = Modifier.fillMaxSize()){
         val (boxRed, boxMagenta, boxBlack, boxCyan, boxYellow) = createRefs()
+
+        Box (modifier = Modifier.size(150.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            bottom.linkTo(parent.bottom)
+            end.linkTo(parent.end)
+        })
+        Box (modifier = Modifier.size(150.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            bottom.linkTo(boxRed.top)
+            start.linkTo(boxRed.end)
+        })
+        Box (modifier = Modifier.size(150.dp).background(Color.Black).constrainAs(boxBlack){
+            top.linkTo(boxRed.bottom)
+            end.linkTo(boxRed.start)
+        })
+        Box (modifier = Modifier.size(150.dp).background(Color.Cyan).constrainAs(boxCyan){
+            bottom.linkTo(boxRed.top)
+            end.linkTo(boxRed.start)
+        })
+        Box (modifier = Modifier.size(150.dp).background(Color.Yellow).constrainAs(boxYellow){
+            top.linkTo(boxRed.bottom)
+            start.linkTo(boxRed.end)
+        })
 
     }
 }
